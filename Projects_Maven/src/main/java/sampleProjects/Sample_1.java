32463f7c79a2f986a7eef8b2e31bbbe57a7c17f5
SANDHARA <SANDHARA@SANDHARA>
2024-02-12 20:27:14 +0530

    updated script

---
+++ Projects_Maven/src/main/java/sampleProjects/Sample_1.java
@@ -0,0 +1,19 @@
+package sampleProjects;
+
+public class Sample_1 {
+	public static void main(String[] args) {
+		
+	
+	StringBuilder obj =new StringBuilder("Sandeep");
+	//obj.delete(1,3);
+	obj.append("ss");
+	//	obj.insert(1,"   ");//D   haraSandeep
+//	obj.replace(1,5,"kkk");//DkkkaraSandeep
+//	obj.delete(0,5);
+//	obj.delete(0, 1);
+	System.out.println(obj);  //
+	
+	}
+	
+
+}
